834d3b377013cd979ffebb4bb2e6a92bb1a8b73c
Yassine <152300833+jitting@users.noreply.github.com>
2025-12-18 00:35:21 +0100

    Added urls.py with submit and result paths

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,22 +1,19 @@
-"""
-URL configuration for onlinecourse project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = 'onlinecourse'
 
 urlpatterns = [
+    # Admin Site
     path('admin/', admin.site.urls),
+
+    # Course Details
+    path('course/<int:course_id>/', views.course_details, name='course_details'),
+
+    # Task 6: Submit Exam
+    path('course/<int:course_id>/submit/', views.submit, name='submit'),
+
+    # Task 6: Exam Result
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='submission_result'),
 ]
